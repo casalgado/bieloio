5fd6edddacba8821b4f11f9bbe4b61bebcd1c72b
casalgado <casalgado86@gmail.com>
2020-08-04 08:38:40 -0500

    barebones structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,33 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Landing",
+    component: () => import("../views/Landing.vue"),
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    component: () => import("../views/About.vue"),
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: () => import("../views/Contact.vue"),
+  },
+  {
+    path: "/content",
+    name: "Content",
+    component: () => import("../views/Content.vue"),
+  },
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
 });
 
 export default router;
---
+++ src/views/Landing.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+export default {
+  name: "Landing",
+  components: {},
+};
+</script>
