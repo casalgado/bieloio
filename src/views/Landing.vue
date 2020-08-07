aba73e500a3153aa8868cc607c082823c87f72ae
casalgado <casalgado86@gmail.com>
2020-08-07 16:16:30 -0500

    starting animation

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home" @click="drawLine">
     <div id="oc">
       <img :src="outerCircle" alt="" />
     </div>
@@ -7,7 +7,26 @@
       <img :src="innerCircle" alt="" />
     </div>
     <div id="li">
-      <img :src="line" alt="" />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1116.98 919">
+        <defs>
+          <mask id="liMask">
+            <rect
+              id="innerLiMask"
+              x="-919"
+              y="-198"
+              width="919"
+              height="1117"
+              fill="#fff"
+            />
+          </mask>
+        </defs>
+        <g id="li-reveal" mask="url(#liMask)">
+          <path
+            class="path-li"
+            d="M955.05,714.76C733.86,428.15,505.12,131.78,134,27.35A938.3,938.3,0,0,0,0,0V121.12a817.65,817.65,0,0,1,101.46,21.67c333,93.69,549.37,374.07,758.79,645.44,33.61,43.56,67.47,87.43,102,130.77H1117C1061.46,852.65,1008,783.32,955.05,714.76Z"
+          />
+        </g>
+      </svg>
     </div>
 
     <p id="slogan" class="landing-text">
@@ -19,6 +38,7 @@
 </template>
 
 <script>
+import { gsap } from "gsap";
 export default {
   name: "Landing",
   components: {},
@@ -33,6 +53,17 @@ export default {
       return require(`../assets/Line.svg`);
     },
   },
+  methods: {
+    drawLine: function() {
+      console.log("this");
+      gsap.to("#innerLiMask", {
+        duration: 0.5,
+        rotation: 90,
+        transformOrigin: "bottom right",
+      });
+    },
+  },
+  mounted() {},
 };
 </script>
 <style scoped>
@@ -41,6 +72,8 @@ export default {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
+  max-width: 1100px;
+  margin: 0 auto;
 }
 
 .home div {
@@ -78,7 +111,7 @@ export default {
   left: 0px;
 }
 
-#li img {
+#li svg {
   position: absolute;
   width: 100%;
   left: 0px;
@@ -102,5 +135,9 @@ export default {
   text-align: right;
   text-transform: uppercase;
   font-family: "agrandir-variableregular";
+}
+
+.path-li {
+  fill: #1c2628;
 }
 </style>
