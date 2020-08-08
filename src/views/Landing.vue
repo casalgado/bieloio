8faa312dc8d7af3b8c3a602f326b6b8725c2e93e
casalgado <casalgado86@gmail.com>
2020-08-07 21:28:55 -0500

    animation done

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,17 +1,57 @@
 <template>
-  <div class="home" @click="drawLine">
+  <div class="home">
     <div id="oc">
-      <img :src="outerCircle" alt="" />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 963.17 844.23">
+        <defs>
+          <mask id="ocMaskContainer">
+            <rect
+              id="ocMask"
+              x="-319"
+              y="-964"
+              width="1282"
+              height="964"
+              fill="#fff"
+            />
+          </mask>
+        </defs>
+        <g id="oc-reveal" mask="url(#ocMaskContainer)">
+          <path
+            class="custom-path"
+            d="M963.17,695q-86.88,23.26-188.52,28.12-23.6,1.11-46.55,1.17h0c-166.87-.51-304.94-41.14-414.25-123.49C193.44,510.17,128.47,381.06,120.72,217Q115.07,98.23,151.44,0H24.82Q-4.7,103,.91,222.72C10.46,424,91.46,583.48,241.67,696.59c130,98,292.8,147.64,484,147.64h2.77q25.59,0,51.92-1.28,96.87-4.61,182.86-24.31Z"
+          />
+        </g>
+      </svg>
     </div>
     <div id="ic">
-      <img :src="innerCircle" alt="" />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 525.58 450.73">
+        <defs>
+          <mask id="icMaskContainer">
+            <rect
+              id="icMask"
+              x="0"
+              y="0"
+              width="526"
+              height="451"
+              fill="#fff"
+            />
+          </mask>
+        </defs>
+        <g id="ic-reveal" mask="url(#icMaskContainer)">
+          <path
+            id="icPath"
+            class="custom-path"
+            transform="translate(526,-450)"
+            d="M81.29,0c-1.52,1.35-3,2.7-4.54,4.08Q-5.42,79.91.38,201.32T95.05,389.75q89,67,228.36,60.39,124-5.88,202.17-66V0Z"
+          />
+        </g>
+      </svg>
     </div>
     <div id="li">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1116.98 919">
         <defs>
-          <mask id="liMask">
+          <mask id="liMaskContainer">
             <rect
-              id="innerLiMask"
+              id="liMask"
               x="-919"
               y="-198"
               width="919"
@@ -20,9 +60,9 @@
             />
           </mask>
         </defs>
-        <g id="li-reveal" mask="url(#liMask)">
+        <g id="li-reveal" mask="url(#liMaskContainer)">
           <path
-            class="path-li"
+            class="custom-path"
             d="M955.05,714.76C733.86,428.15,505.12,131.78,134,27.35A938.3,938.3,0,0,0,0,0V121.12a817.65,817.65,0,0,1,101.46,21.67c333,93.69,549.37,374.07,758.79,645.44,33.61,43.56,67.47,87.43,102,130.77H1117C1061.46,852.65,1008,783.32,955.05,714.76Z"
           />
         </g>
@@ -33,7 +73,7 @@
       Historias que el <br />
       mundo debe escuchar
     </p>
-    <h2 id="logo-text" class="landing-text">BIELO</h2>
+    <p id="logo-text" class="landing-text">BIELO</p>
   </div>
 </template>
 
@@ -54,16 +94,42 @@ export default {
     },
   },
   methods: {
-    drawLine: function() {
-      console.log("this");
-      gsap.to("#innerLiMask", {
-        duration: 0.5,
+    animate: function() {
+      gsap.to("#liMask", {
+        duration: 0.8,
         rotation: 90,
         transformOrigin: "bottom right",
       });
+      gsap
+        .to("#ocMask", {
+          duration: 0.8,
+          rotation: -90,
+          transformOrigin: "bottom right",
+        })
+        .delay(0.4);
+      gsap
+        .to("#icPath", {
+          duration: 1,
+          transform: "translate(0,0)",
+        })
+        .delay(0.7);
+      gsap
+        .to("#logo-text", {
+          duration: 1,
+          opacity: 1,
+        })
+        .delay(0.7);
+      gsap
+        .to("#slogan", {
+          duration: 2,
+          opacity: 1,
+        })
+        .delay(1.7);
     },
   },
-  mounted() {},
+  mounted() {
+    this.animate();
+  },
 };
 </script>
 <style scoped>
@@ -72,7 +138,7 @@ export default {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
-  max-width: 1100px;
+
   margin: 0 auto;
 }
 
@@ -85,6 +151,7 @@ export default {
   height: 42.4%;
   top: 0px;
   right: 0px;
+  max-width: 361px;
 }
 
 #ic img {
@@ -97,6 +164,7 @@ export default {
   height: 79.4%;
   top: 0px;
   right: 0px;
+  max-width: 663px;
 }
 
 #oc img {
@@ -109,6 +177,7 @@ export default {
   height: 86.4%;
   bottom: 0px;
   left: 0px;
+  max-width: 768px;
 }
 
 #li svg {
@@ -122,12 +191,14 @@ export default {
   position: absolute;
   margin: 30px;
   bottom: 0px;
+  opacity: 0;
 }
 
 #logo-text {
   left: 0px;
   font-weight: 1000;
   font-family: "agrandir-wideheavy";
+  font-size: 250%;
 }
 
 #slogan {
@@ -135,9 +206,10 @@ export default {
   text-align: right;
   text-transform: uppercase;
   font-family: "agrandir-variableregular";
+  font-size: 140%;
 }
 
-.path-li {
+.custom-path {
   fill: #1c2628;
 }
 </style>
